b21aac16c24a757c56637c1547a81abd81f54710
B-enah <gikonyobenard01@gmail.com>
2026-06-20 14:44:22 +0300

    Added the HeroSection

--- src/app/features/landingPage/page.tsx
+++ src/app/features/landingPage/page.tsx
@@ -1,11 +1,13 @@
 import About from "./components/About"
 import Footer from "./components/Footer"
-
+import Hero from "./components/HeroSection"
 
 export default function LandingPage() {
   return (
 <>
+<Hero />
 <About />
 <Footer />
+
 </>)
 }
