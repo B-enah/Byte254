f870d36403b2f3c61ffa8330c2032fa506b0aaa9
B-enah <gikonyobenard01@gmail.com>
2026-06-21 00:34:11 +0300

    Restructed the dir

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,17 @@
-// src/app/page.tsx
-import LandingPage from "./features/landingPage/page"
+import HeroSection from "@/features/landingPage/components/HeroSection"
+import About from "@/features/landingPage/components/About"
+import ContactUs from "@/features/landingPage/components/ContactUs"
+import Footer from "@/features/landingPage/components/Footer"
+import WhyUs from "@/features/landingPage/components/WhyUs"
 
 export default function Home() {
-  return <LandingPage />
+  return (
+    <main>
+      <HeroSection />
+      <About />
+      <WhyUs />
+      <ContactUs />
+      <Footer />
+    </main>
+  )
 }
